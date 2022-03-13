d9c886f6a956a26fce7624b433c183714aaf234f
Zax71 <zabax32@gmail.com>
2022-03-13 18:52:08 +0000

    Add favicon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './index.css';
 
 function App() {
   return (
-    <h1 className="text-green-300 text-5xl">Endercube</h1>
+    <div>
+      <h1 className="text-green-300 text-5xl">Endercube</h1>
+    </div>
+    
   );
 }
 
