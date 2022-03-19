1af99e7630801500ef3053cca193ebabb278b572
Zax71 <zabax32@gmail.com>
2022-03-19 14:39:26 +0000

    Change colors (again)

--- src/Nav.js
+++ src/Nav.js
@@ -10,11 +10,11 @@ function Nav() {
 
       <ul className='flex'>
         <li className='inline-block'><img src={EndercubeLogo} alt="" className='ml-1 mt-auto items-center' height={50} width={50} /></li>
-        <li className='inline self-end'><a href="#home" className=' ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-md shadow-slate-800'>Home</a></li>
-        <li className='inline self-end'><a href="#about" className='ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-md shadow-slate-800'>About</a></li>
-        <li className='inline self-end'><a href="#map" className='ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-md shadow-slate-800'>Map</a></li>
-        <li className='inline self-end'><a href="#documentation" className='ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-md shadow-slate-800'>Documentation</a></li>
-        <li className='inline self-end'><a href="#discord" className='ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-md shadow-slate-800'>Discord</a></li>
+        <li className='inline self-end'><a href="#home" className=' ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-sm shadow-gray-6'>Home</a></li>
+        <li className='inline self-end'><a href="#about" className='ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-sm shadow-gray-6'>About</a></li>
+        <li className='inline self-end'><a href="#map" className='ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-sm shadow-gray-6'>Map</a></li>
+        <li className='inline self-end'><a href="#documentation" className='ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-sm shadow-gray-6'>Documentation</a></li>
+        <li className='inline self-end'><a href="#discord" className='ml-2 px-4 py-1 text-lg font-semibold text-gray-0 bg-gray-2 hover:bg-gray-3 rounded-sm shadow-sm shadow-gray-6'>Discord</a></li>
       </ul>
     </nav>
   );
