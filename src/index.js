c6e3c066d96906d9cc5ac548f5dd112b98f7f023
Zax71 <zabax32@gmail.com>
2022-04-03 18:33:02 +0100

    Remove useless files from CRA

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import About from './pages/About.js';
 import Documentation from './pages/Documentation.js';
 import Staff from './pages/Staff.js';
 
-import reportWebVitals from './reportWebVitals';
 import Nav from './components/Nav';
 import GitHubLink from './components/GitHubLink.js';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -29,8 +28,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
