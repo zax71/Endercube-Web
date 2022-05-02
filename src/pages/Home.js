dfadbf66ad33bcbb08a6cf952bd0125a9e98050c
Zax71 <zabax32@gmail.com>
2022-05-02 08:01:58 +0100

    Add online players counter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,37 +2,37 @@ import '../index.css';
 
 import ECLogoTransparent from '../images/EndercubeLogoTransparent.png'
 import webBg from '../images/webBg.png'
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 
+// var online = fetch('https://api.mcsrvstat.us/2/mc.endercube.net')
+  // .then(response => response.json())
+  // .then(data => console.log(data['players']['online']));
 
 
 function App() {
+  const url = 'https://api.mcsrvstat.us/2/mc.endercube.net'
+  const [product, setProduct] = useState(null)
 
-  // API calls
-  // // var online = fetch('https://api.mcsrvstat.us/2/mc.endercube.net')
-  // // .then(response => response.json())
-  // // .then(data => console.log(data['players']['online']));
-  // const [server, setServer] = useState(null);
-  // const [loading, setLoading] = useState(true);
+  useEffect(() => {
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const response = await fetch('https://api.mcsrvstat.us/2/mc.endercube.net');
-  //     const data = await response.json();
-  //     const item = data.results[0];
-  //     console.log(item);
-  //     setServer(item);
-  //     setLoading(false);
-  //   }
-  //   fetchData();
-  // }, [])
-
+    axios.get(url)
+      .then(response => {
+        setProduct(response.data)
+      })
+  
+    
+  }, [url])
+  
   return (
+    <div>
       <div className='border-y-2 border-gray-0 flex justify-center align-middle relative'>
         <img src={webBg} alt="" className='' />
-        {/* {loading ? <div>Loading...</div> : <div>{server.fetchData.players.online}</div>} */}
-        {/* <p>Player count (placeholder for api call)</p> */}
+        <p className='text-gray-0 font-openSans text-xl absolute top-[15%] left-[20vw]'>Online Players: {product == null ? "Loading" : product.players.online}</p>
         <img src={ECLogoTransparent} alt="" width="150" height="150" className='border-4 rounded-[25%] border-gray-0 absolute top-[15%]'/>
       </div>
+      
+    </div>
       
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {
       backgroundImage: {
         'webBg': "url('./images/webBg.png')"
+      },
+      fontFamily: {
+        openSans: ['Open Sans', 'sans-serif']
       }
     },
     colors: {
