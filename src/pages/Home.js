941af347ba4a92708e4e5a89e537336f6ba6b0ce
Zax71 <zabax32@gmail.com>
2022-06-10 16:59:04 +0100

    Add some content...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,11 +26,34 @@ function App() {
   
   return (
     <div>
-      <div className='border-y-2 border-gray-0 flex justify-center align-middle relative'>
+      <div className='border-y-2 border-gray-0 flex justify-center align-middle relative'> {/*Top banner*/}
         <img src={webBg} alt="" className='' />
         <p className='text-gray-0 font-openSans text-xl absolute top-[15%] left-[20vw]'>Online Players: {product == null ? "Loading" : product.players.online}</p>
         <img src={ECLogoTransparent} alt="" width="150" height="150" className='border-4 rounded-[25%] border-gray-0 absolute top-[15%]'/>
       </div>
+      
+      <div className='mr-auto ml-auto w-[80%] mt-3'> {/*Centered container - everything in here*/}
+
+        <div className='text-gray-0 font-openSans pb-2 mb-6 border-b-4 border-b-gray-0'> {/*About us container - full width*/}
+          <h2 className='font-oswald text-2xl'>About Us</h2>
+          <p className='text-left text-lg'>Welcome to Endercube, a modern Minecraft server that focusses on creating a fresh and unique survival experience for all with a selection of new Minecraft features being used to their full potential and old standards still being conserved to create a familiar experience with a twist, paving the way through to a new era of Minecraft servers.</p>
+        </div>
+
+        <div className='grid grid-cols-2 gap-3'> {/*Features grid container*/}
+          <div className='text-gray-0 font-openSans'> {/*Enchanting container*/}
+            <p className='text-lg'>Enchanting has never been more fun! With our over 200 custom enchantments you'll never run out of ideas</p>
+          </div>
+          <div className='text-gray-0 font-openSans'> {/*Economy container*/}
+            <p className='text-lg'>We have an entirely player based economy so no more hyper-inflation and easy wins. You've got to work for your money</p>
+          </div>
+          <div className='text-gray-0 font-openSans'> {/*Enchanting container*/}
+            <p className='text-lg'>We have multiple game modes alongside survival too! Parkour and Creative to develop your builds</p>
+          </div>
+        </div>
+        
+
+      </div>
+      
       
     </div>
       
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,13 @@ module.exports = {
         'webBg': "url('./images/webBg.png')"
       },
       fontFamily: {
-        openSans: ['Open Sans', 'sans-serif']
+        openSans: ['Open Sans', 'sans-serif'],
+        oswald: ['Oswald', 'sans-serif']
       }
     },
     colors: {
       'gray-0': '#ffffff',
-      'gray-1': '##4a4a4a',
+      'gray-1': '#4a4a4a',
       'gray-2': '#363636',
       'gray-3': '#2f2f2f',
       'gray-4': '#212121',
