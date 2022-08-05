7d3eb28154dc5b4c3274fb909c08763404244709
Zax71 <zabax32@gmail.com>
2022-08-05 15:38:09 +0100

    Fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { ToastContainer } from 'react-toastify';
 function footer() {
     return (
         <div >
-            <p className='text-gray-0 bg-gray-4 text-sm p-2 font-inter fixed bottom-0'>Endercube is not affiliated with Minecraft, Mojang AB and/or Microsoft Corporation</p>
+            <p className='text-gray-0 bg-gray-4 text-sm p-2 font-inter bottom-0 w-[100%]'>Endercube is not affiliated with Minecraft, Mojang AB and/or Microsoft Corporation</p>
             <GitHubLink />
             <ToastContainer/>
             
